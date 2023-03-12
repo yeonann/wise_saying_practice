992b97a6784b3123113dee1a04ec49c0ab130c70
yeonann <lkim82@naver.com>
2023-03-13 01:17:25 +0900

    4단계

--- src/main/java/io/babyLion/wiseSaying/App.java
+++ src/main/java/io/babyLion/wiseSaying/App.java
@@ -12,6 +12,7 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+        int number = 1;
 
         while (true) {
             System.out.print("명령) ");
@@ -25,7 +26,8 @@ public class App {
                 System.out.print("작가 : ");
                 sc.nextLine().trim();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.printf("%d번 명언이 등록되었습니다.\n", number);
+                number++;
             }
         }
     }
