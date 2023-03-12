1508a6c4600ea21b8f43cbcda398fa046ecba8d3
yeonann <lkim82@naver.com>
2023-03-13 01:33:29 +0900

    5단계

--- src/main/java/io/babyLion/wiseSaying/App.java
+++ src/main/java/io/babyLion/wiseSaying/App.java
@@ -1,5 +1,7 @@
 package io.babyLion.wiseSaying;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -11,8 +13,11 @@ public class App {
     }
 
     public void run() {
+
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+
         System.out.println("== 명언 앱 ==");
-        int number = 1;
+        int number = 0;
 
         while (true) {
             System.out.print("명령) ");
@@ -21,13 +26,27 @@ public class App {
             if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
-                System.out.print("명언 : ");
-                sc.nextLine().trim();
-                System.out.print("작가 : ");
-                sc.nextLine().trim();
+                int id = number + 1;
 
-                System.out.printf("%d번 명언이 등록되었습니다.\n", number);
-                number++;
+                System.out.print("명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String authorName = sc.nextLine().trim();
+
+                WiseSaying wiseSaying = new WiseSaying(id, content, authorName);
+                wiseSayings.add(wiseSaying);
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                number = id;
+
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-".repeat(20));
+
+                for (WiseSaying wiseSaying : wiseSayings) {
+                    System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthorName(), wiseSaying.getContent());
+                }
+
             }
         }
     }
---
+++ src/main/java/io/babyLion/wiseSaying/WiseSaying.java
@@ -0,0 +1,26 @@
+package io.babyLion.wiseSaying;
+
+public class WiseSaying {
+
+    private int id;
+    private String content;
+    private String authorName;
+
+    public WiseSaying(int id, String content, String authorName) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+}
