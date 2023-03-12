8da9eadb998e3e359e36bcc1b16860f99c907aa2
yeonann <lkim82@naver.com>
2023-03-13 01:11:42 +0900

    1단계

---
+++ src/main/java/io/babyLion/wiseSaying/Main.java
@@ -0,0 +1,13 @@
+package io.babyLion.wiseSaying;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        new App(sc).run();
+
+        sc.close();
+    }
+}
