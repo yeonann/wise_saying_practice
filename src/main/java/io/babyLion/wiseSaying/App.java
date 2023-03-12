19d085aeaadc4daac8501a89297a0e045f9ad220
yeonann <lkim82@naver.com>
2023-03-13 02:08:54 +0900

    명령어 파싱

--- src/main/java/io/babyLion/wiseSaying/App.java
+++ src/main/java/io/babyLion/wiseSaying/App.java
@@ -3,6 +3,9 @@ package io.babyLion.wiseSaying;
 import io.babyLion.wiseSaying.system.controller.SystemController;
 import io.babyLion.wiseSaying.wiseSay.controller.WiseSayController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class App {
 
@@ -24,6 +27,22 @@ public class App {
                 wiseSayController.write();
             } else if (command.equals("목록")) {
                 wiseSayController.list();
+            } else if (command.startsWith("삭제")) {
+                String[] commandBits = command.split("\\?",2);
+                String actionCode = commandBits[0];
+                Map<String, String> parameters = new HashMap<>();
+                String[] parameterBits = commandBits[1].split("&");
+
+                for ( String parameter : parameterBits) {
+                    String[] parameterBit = parameter.split("=", 2);
+                    String key = parameterBit[0];
+                    String value = parameterBit[1];
+                    parameters.put(key,value);
+                }
+
+                System.out.printf("%s\n",actionCode);
+                System.out.printf("%s\n",parameters);
+                System.out.println("명언이 삭제되었습니다.");
             }
         }
     }
